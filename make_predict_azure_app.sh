80c50b6d858a03c47187e2f7e68da624a63899d3
Kanbar <Mikdad.Kanbar@shell.com>
2024-03-03 23:17:40 +0100

    e

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://webappudacity22231.azurewebsites.net:$PORT/predict 
+     -X POST https://webappudacity22231.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
